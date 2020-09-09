c273b069541a94635778eb658644f435d434bd17
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2020-09-09 12:29:04 +0800

    added README

---
+++ README.md
@@ -0,0 +1,15 @@
+TODO :
+
+9 September 2020
+
+1. Home Screen DONE
+2. Video Screen DONE
+3. Camera View :
+   1. Load model
+   2. Check if loading of model is done
+   3. If loading is done then display a pop up
+   4. Start button will be attached to the model to make predictions
+   5. Prediction function will take input from the camera and convert it to frames
+   6. Each frame is passed through the model
+   7. Model Prediction View will be showing the model predictions for each class and the confidence it has
+   8. Stop model will stop the model making predictions
--- screens/CameraView.js
+++ screens/CameraView.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect } from "react";
+import { Button, Text, View } from "react-native";
+import { Camera } from "expo-camera";
 
-import styles from "../styles/style"
+import styles from "../styles/style";
 
 export default function CameraView() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -11,7 +11,7 @@ export default function CameraView() {
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
@@ -27,31 +27,26 @@ export default function CameraView() {
         <View
           style={{
             flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
+            backgroundColor: "transparent",
+            flexDirection: "row",
             justifyContent: "center"
-          }}>
+          }}
+        >
           <View style={styles.modelButtonContainer}>
-          <Button 
-            title= "Flip Screen"
-            color ="black"
-            style={styles.appButtonText2}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}
-          />
-          <Button 
-            title= "Start"
-            color="green"
-          />
-          <Button 
-            title= "Stop"
-            color="red"
-          />
+            <Button
+              title="Flip Screen"
+              color="black"
+              style={styles.appButtonText2}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}
+            />
+            <Button title="Start" color="green" />
+            <Button title="Stop" color="red" />
           </View>
         </View>
       </Camera>
@@ -59,15 +54,15 @@ export default function CameraView() {
   );
 }
 // Need to add two buttons for starting the model and one for stopping the model
-            //<View style={styles.buttonContainer}>
-            //<Button
-            //  title="Start Driving!"
-            //  color="green"
-            //  onPress={()=> navigation.navigate("VideoScreen")}
-            ///>
-            //<Button
-            //  title="Stop Driving!"
-            //  color="red"
-            //  onPress={()=> navigation.navigate("VideoScreen")}
-            ///>
-            //</View>
+//<View style={styles.buttonContainer}>
+//<Button
+//  title="Start Driving!"
+//  color="green"
+//  onPress={()=> navigation.navigate("VideoScreen")}
+///>
+//<Button
+//  title="Stop Driving!"
+//  color="red"
+//  onPress={()=> navigation.navigate("VideoScreen")}
+///>
+//</View>
